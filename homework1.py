7acc4a36e71ad1d7a50266856b4533b29cfb61af
Fake-mask <937912646@qq.com>
2020-08-12 10:09:12 +0800

    Code quality commit

--- homework1.py
+++ homework1.py
@@ -1,21 +1,24 @@
 import tensorflow as tf
 
+# 从mnist数据集提取数据并定义训练集输入和测试集输入
 mnist = tf.keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train, x_test = x_train / 255.0, x_test / 255.0
+x_train, x_test = x_train / 255.0, x_test / 255.0   #归一化处理
 
+#利用Sequential搭建神经网络
 model = tf.keras.models.Sequential([
     tf.keras.layers.Flatten(),
     tf.keras.layers.Dense(128, activation='relu'),
     tf.keras.layers.Dense(10, activation='softmax')
 ])
 
+#利用compile定义优化器、损失函数和输出方法
 model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
               metrics=['sparse_categorical_accuracy'])
 
+#定义相关数据参数
 model.fit(x_train, y_train, batch_size=32, epochs=5, validation_data=(x_test, y_test), validation_freq=1)
+
+#打印神经网络结构
 model.summary()
-# first commit
-# second commit
-# third commit
